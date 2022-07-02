be4eafeb3ed6d29308f349e24ec33b79f5d08a0a
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-07-02 23:21:01 +0000

    console

---
+++ console.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Class HBNBComand"""
+
+
+import cmd
+import json
+from models.base_model import BaseModel
+
+
+class HBNBCommand(cmd.Cmd):
+    """Simple command processor"""
+    prompt = '(hbnb)'
+
+    def do_EOF(self, arg):
+        """end of file"""
+        return True
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def emptyline(self):
+        """empty line"""
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
