cd3e358bc5212e7f51e2adc10970251d78492508
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-07-03 14:54:50 +0000

    test base model

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Test_BaseModel module"""
+
+
 from models.base_model import BaseModel
 from models import *
 import unittest
-import os
 from datetime import datetime
 import uuid
 
@@ -20,28 +21,20 @@ class Test_BaseModel(unittest.TestCase):
 
     def test_attributes(self):
         """Check for attributes."""
-        b1 = BaseModel()
-        self.assertEqual(str, type(b1.id))
-        self.assertEqual(datetime, type(b1.created_at))
-        self.assertEqual(datetime, type(b1.updated_at))
+        base1 = BaseModel()
+        self.assertEqual(str, type(base1.id))
+        self.assertEqual(datetime, type(base1.created_at))
+        self.assertEqual(datetime, type(base1.updated_at))
 
-        b2 = BaseModel(id=4, created_at='2017-06-14T22:31:03.285259')
-        self.assertEqual(b2.id, 4)
-        self.assertEqual(datetime, type(b2.created_at))
+        base2 = BaseModel(id=4, created_at='2017-06-14T22:31:03.285259')
+        self.assertEqual(base2.id, 4)
+        self.assertEqual(datetime, type(base2.created_at))
 
     def test_str(self):
         """test the str output"""
         b3 = BaseModel()
         string = f"[{b3.__class__.__name__}] ({b3.id}) {b3.__dict__}"
         self.assertEqual(str(b3), string)
-
-    def test_save(self):
-        """test if the object is saved in file"""
-        b4 = BaseModel()
-        b4.save()
-        storage.reload()
-        objects = storage.all()
-        self.assertEqual(b4.id, objects[f"{b4.__class__.__name__}.{b4.id}"].id)
 
     def test_todict(self):
         """test if the object is converted to dict"""
@@ -50,7 +43,6 @@ class Test_BaseModel(unittest.TestCase):
         self.assertEqual(mydict['__class__'], 'BaseModel')
         self.assertEqual(type(mydict['created_at']), str)
         self.assertEqual(type(mydict['updated_at']), str)
-
 
 
 if __name__ == "__main__":
